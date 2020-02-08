33f69ce66f1c4d1ea1a3b132f9a45f4b7e399a98
Jorge Moura <jjorge.moura@gmail.com>
2020-02-08 13:28:28 +0000

    Initial Typography work

---
+++ toscoz/Application/Foundation/Fonts.swift
@@ -0,0 +1,9 @@
+//
+//  Fonts.swift
+//  toscoz
+//
+//  Created by Jorge Moura on 06/02/2020.
+//  Copyright © 2020 Jorge Moura. All rights reserved.
+//
+
+import Foundation
---
+++ toscoz/Application/Foundation/UIColor+App.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  toscoz
+//
+//  Created by Jorge Moura on 05/02/2020.
+//  Copyright © 2020 Jorge Moura. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+//    convenience init(colorSetName: String) {
+//        self.init(named: colorSetName) ?? UIColor.blue
+//    }
+
+//    static var appPrimary: UIColor {
+//        UIColor(colorSetName: "toscoGreen")
+//    }
+
+    static var appLink: UIColor {
+        .link
+    }
+}
